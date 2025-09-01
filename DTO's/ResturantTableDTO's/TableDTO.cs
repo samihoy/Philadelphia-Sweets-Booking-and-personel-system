fc254db5de5862aacfd4048ab323294f7e36560e
Bror Lundberg <maximillian.lundber@chasacademy.se>
2025-09-01 03:01:04 +0200

    Fresh start

---
+++ DTO's/ResturantTableDTO's/TableDTO.cs
@@ -0,0 +1,14 @@
+﻿using Philadelphia_Sweets_booking_System__Resturant_.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Philadelphia_Sweets_booking_System__Resturant_.DTO_s.ResturantTableDTO_s
+{
+    public class TableDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int Seats { get; set; }
+        public bool IsAvalible { get; set; }
+    }
+}
---
+++ Repositorys/IRepositorys/ITableRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Philadelphia_Sweets_booking_System__Resturant_.Models;
+using Table = Philadelphia_Sweets_booking_System__Resturant_.Models.Table;
+
+namespace Philadelphia_Sweets_booking_System__Resturant_.Repositorys.IRepository
+{
+    public interface ITableRepository
+    {
+        Task<List<Table>> GetAllTablesAsync();
+        Task<Table?> GetTableByIdAsync(int id);
+        //-----------------------------------------------
+        Task<int> AddTableAsync(Table table);
+        Task<bool> EditTableAsync(Table table);
+        //-----------------------------------------------
+        Task<bool> DeleteTableAsync(int tableId);
+        Task<int> DeleteTablesAsync(List<int> tableIds);
+    }
+}
