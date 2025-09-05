b6b58e72d863986d9e8e23c9c117a50fdc98431c
samihoy <maximillian.lundberg@chasacademy.se>
2025-09-05 03:08:31 +0200

    I hope this works

--- Models/my own stuff/Schedule.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Philadelphia_Sweets_booking_System__Resturant_.Models
-{
-    public class Schedule
-    {
-        public int Id { get; set; }
-        public ICollection<Shift> Shifts { get; set; } = new List<Shift>();
-    }
-}
