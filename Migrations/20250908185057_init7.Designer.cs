d561340dc088c63055302ac75ba17cbd827d6f4a
samihoy <maximillian.lundberg@chasacademy.se>
2025-09-08 22:18:26 +0200

    finishing up and fixing some lose points

--- Migrations/20250908185057_init7.Designer.cs
+++ Migrations/20250908185057_init7.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Philadelphia_Sweets_booking_System__Resturant_.Data;
 
@@ -11,9 +12,11 @@ using Philadelphia_Sweets_booking_System__Resturant_.Data;
 namespace Philadelphia_Sweets_booking_System__Resturant_.Migrations
 {
     [DbContext(typeof(PhiladelphiaSweetsResturandDBContext))]
-    partial class PhiladelphiaSweetsResturandDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250908185057_init7")]
+    partial class init7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
