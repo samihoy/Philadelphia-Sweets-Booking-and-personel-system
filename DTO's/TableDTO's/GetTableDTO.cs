99a484ffadb4206748e269421bdebff29471fb57
samihoy <maximillian.lundberg@chasacademy.se>
2025-09-08 22:16:06 +0200

    added some new DTos for tables

---
+++ DTO's/TableDTO's/GetTableDTO.cs
@@ -0,0 +1,13 @@
+﻿using Philadelphia_Sweets_booking_System__Resturant_.DTO_s.BookingDTO_s;
+using Philadelphia_Sweets_booking_System__Resturant_.Models;
+
+namespace Philadelphia_Sweets_booking_System__Resturant_.DTO_s.TableDTO_s
+{
+    public class GetTableDTO
+    {
+        public int Id { get; set; }
+        public int TableNumber { get; set; }
+        public int Seats { get; set; }
+        public ICollection<GetBookingDTO> Bookings { get; set; }
+    }
+}
