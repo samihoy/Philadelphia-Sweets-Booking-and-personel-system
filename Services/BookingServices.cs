c50c0eb45b6aba89c5e03fd963b4ea4c04a977cc
samihoy <maximillian.lundberg@chasacademy.se>
2025-09-08 03:56:08 +0200

    updated and rewrote some services

--- Services/BookingServices.cs
+++ Services/BookingServices.cs
@@ -1,58 +1,135 @@
-﻿using Philadelphia_Sweets_booking_System__Resturant_.DTO_s.ResturantBookingDTO_s;
+﻿
+using Philadelphia_Sweets_booking_System__Resturant_.DTO_s.BookingDTO_s;
 using Philadelphia_Sweets_booking_System__Resturant_.Models;
+using Philadelphia_Sweets_booking_System__Resturant_.Repositorys.IRepository;
 using Philadelphia_Sweets_booking_System__Resturant_.Repositorys.IRepositorys;
 using Philadelphia_Sweets_booking_System__Resturant_.Services.IServices;
 
 namespace Philadelphia_Sweets_booking_System__Resturant_.Services
 {
-    public class BookingServices :IBookingService
+    public class BookingServices : IBookingService
     {
-        private readonly IbookingRepository _bookingrepo;
+        private readonly IBookingRepository _bookingrepo;
+        private readonly ITableRepository _tablerepo;
 
-        public BookingServices(IbookingRepository repo)
+        public BookingServices(IBookingRepository bookingRepo, ITableRepository tableRepo)
         {
-            _bookingrepo = repo;
+            _bookingrepo = bookingRepo;
+            _tablerepo = tableRepo;
         }
-
-        public Task<int> CreateBookingDTO(CreateBookingDTO DTO)
+        public async Task<int> CreateBookingServicesAsync(CreateBookingDTO DTO, List<int> tableIds)
         {
-            var booking = new Booking
+            try
             {
-                StartTime = DTO.StartTime,
-                NumberGuests = DTO.NumberGuests,
-                BookedUnderName = DTO.BookedUnderName,
-                ContactInformationPhone = DTO.ContactInformationPhone,
-                DurationMinutes = DTO.DurationMinutes
-            };
+                var tables = await _tablerepo.RepoGetTablesByIdAsync(tableIds);
 
-            var tableBooking = DTO.TableIds.Select(tb => new TableBooking
+                var booking = new Booking
+                {
+                    StartTime = DTO.StartTime,
+                    NumberGuests = DTO.NumberGuests,
+                    BookedUnderName = DTO.BookedUnderName,
+                    ContactInformationPhone = DTO.ContactInformationPhone,
+                    DurationMinutes = DTO.DurationMinutes,
+                    Tables=tables
+                };
+
+                var tableId = await _bookingrepo.RepoAddAsync(booking);
+
+                return tableId;
+               
+            }
+            catch(Exception ex)
             {
-                TableId = tb,
-                Booking = booking,
-            }).ToList();
-
-            _bookingrepo.AddBookingAsync(booking);
-            
+                throw new InvalidOperationException("Operation failed, booking could not be completed");
+            }
         }
 
-        public Task<List<CreateBookingDTO>> GetAllBookingDTOs()
+        public async Task<int> DeleteBookingServicesAsyc(int bookingId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var rowsDeleted = await _bookingrepo.RepoDeleteAsync(bookingId);
+
+                return rowsDeleted;
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidCastException($"Failed to delete Booking with ID {bookingId},{ex}");
+            }
         }
 
-        public Task<CreateBookingDTO> GetBookingDTO()
+        public async Task<List<BookingsDTO>> GetAllBookingsServicesAsync()
         {
-            throw new NotImplementedException();
+            try
+            { 
+                var allBookings = await _bookingrepo.RepoGetAllAsync();
+
+                var allBookingsDTOs = allBookings.Select(b => new BookingsDTO
+                {
+                    StartTime = b.StartTime,
+                    DurationMinutes = b.DurationMinutes,
+                    TableIds = b.Tables.Select(t=>t.Id).ToList()
+
+                }).ToList();
+
+                return allBookingsDTOs;
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to fetch tables, {ex}");
+            }
         }
 
-        public Task<int> RemovrBookingAsyc(int bookingId)
+        public async Task<GetBookingDTO> GetBookingByIdServicesAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var booking = await _bookingrepo.RepoGetByIdAsync(id);
+
+                var BookingDTO = new GetBookingDTO
+                {
+                    Id = booking.Id,
+                    NumberGuests = booking.NumberGuests,
+                    BookedUnderName = booking.BookedUnderName,
+                    ContactInformationPhone = booking.ContactInformationPhone,
+                    DurationMinutes = booking.DurationMinutes,
+                    StartTime = booking.StartTime,
+                    TableIds = booking.Tables.Select(t => t.Id).ToList()
+
+                };
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidOperationException($"Operation failed could not get booking with ID {id} {ex}");
+            }
         }
 
-        public Task<int> UpdateBooking(CreateBookingDTO booking)
+        public async Task<int> UpdateBookingServicesAsync(UpdateBookingDTO bookingDTO, int id)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                var booking = await _bookingrepo.RepoGetByIdAsync(id);
+
+                booking.DurationMinutes = bookingDTO.DurationMinutes;
+                booking.BookedUnderName = bookingDTO.BookedUnderName;
+                booking.ContactInformationPhone = bookingDTO.ContactInformationPhone;
+                booking.ContactInformationMail = bookingDTO.ContactInformationMail;
+                booking.StartTime = bookingDTO.StartTime;
+                booking.NumberGuests = bookingDTO.NumberGuests;
+                //gör imorgon
+                //booking.Tables = bookingDTO.TablesId.Select()
+
+
+
+
+
+                var updatedBooking = await _bookingrepo.RepoUpdateAsync();
+            }
+            catch(Exception ex)
+            {
+                throw new = InvalidOperationException($"Error operation failed {ex}");
+            }
+        
+        }   
     }
 }
