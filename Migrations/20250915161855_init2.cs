113b3f2ab721de5d8209812bc405ea98938c15dc
samihoy <maximillian.lundberg@chasacademy.se>
2025-09-16 04:58:38 +0200

    last commit before resyncing git mail

---
+++ Migrations/20250915161855_init2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Philadelphia_Sweets_booking_System__Resturant_.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Menues",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateOnly(2025, 9, 15));
+
+            migrationBuilder.UpdateData(
+                table: "Menues",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateOnly(2025, 9, 15));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Menues",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateOnly(2025, 9, 12));
+
+            migrationBuilder.UpdateData(
+                table: "Menues",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateOnly(2025, 9, 12));
+        }
+    }
+}
